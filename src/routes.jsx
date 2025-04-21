a01ac7a3febfa97393f3c6a87a504fbc5835aeb8
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-04-21 14:26:27 +0300

    Set up routes for Home, Actors, Directors, and Movie pages with error handling

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Actors from "./pages/Actors";
+import Directors from "./pages/Directors";
+import Movie from "./pages/Movie";
+import ErrorPage from "./pages/ErrorPage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/actors",
+    element: <Actors />
+  },
+  {
+    path: "/directors",
+    element: <Directors />
+  },
+  {
+    path: "/movie/:id",
+    element: <Movie />
+  }
+]);
 
-const routes = [
-   
-  ];
-
-export default routes;
+export default router;
