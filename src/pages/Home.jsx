25f17e068da2520e5a01f05d09ec4046f3acc14a
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-04-21 14:21:05 +0300

    Create Movie page component to fetch and display movie details based on ID

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,27 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard";
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/movies")
+      .then(r => r.json())
+      .then(data => setMovies(data));
+  }, []);
+
   return (
     <>
-      <header>
-        {/* What component should go here? */}
-      </header>
+      <NavBar />
+      <h1>Home Page</h1>
       <main>
-        {/* Info goes here! */}
+        {movies.map(movie => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
       </main>
     </>
   );
-};
+}
 
 export default Home;
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,16 +1,31 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const [movie, setMovie] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/movies/${id}`)
+      .then(r => r.json())
+      .then(data => setMovie(data));
+  }, [id]);
+
+  if (!movie) return <div>Loading...</div>;
+
   return (
     <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Movie info here! */}
-      </main>
+      <NavBar />
+      <h1>{movie.title}</h1>
+      <p>Time: {movie.time}</p>
+      <div>
+        {movie.genres.map((genre, index) => (
+          <span key={index}>{genre}</span>
+        ))}
+      </div>
     </>
   );
-};
+}
 
 export default Movie;
