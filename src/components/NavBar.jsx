7accdd50698295ed633d48870ceed3961a677b07
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-04-21 14:17:38 +0300

    Create NavBar component with navigation links to Home, Actors, and Directors pages

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,10 +3,12 @@ import "./NavBar.css";
 
 function NavBar() {
   return (
-    <nav className="navbar">
-    
-    </nav>
-    );
-};
+    <div className="navbar">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+    </div>
+  );
+}
 
 export default NavBar;
