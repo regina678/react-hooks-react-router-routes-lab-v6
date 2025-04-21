c49db1d9e918fc1c3824e392c2b29bb766a6d598
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-04-21 14:19:43 +0300

    Create Directors page component to fetch and display a list of directors and their movies

--- src/pages/Directors.jsx
+++ src/pages/Directors.jsx
@@ -1,16 +1,31 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directors, setDirectors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/directors")
+      .then(r => r.json())
+      .then(data => setDirectors(data));
+  }, []);
+
   return (
     <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Director info here! */}
-      </main>
+      <NavBar />
+      <h1>Directors Page</h1>
+      {directors.map(director => (
+        <article key={director.id}>
+          <h2>{director.name}</h2>
+          <ul>
+            {director.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </article>
+      ))}
     </>
   );
-};
+}
 
 export default Directors;
